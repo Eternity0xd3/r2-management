59cf7a92b343c614d6b74c6ebce94d499d0ac0e7
Eternity0xd3 <eternity211@126.com>
2026-08-19 00:54:12 +0800

    feat: r2 management class

---
+++ src/r2_management/r2_management.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+
+import boto3
+
+class R2Management:
+    def __init__(self, bucket_name, endpoint_url, access_key, secret_key, region_name='auto', public_url=None) -> None:
+        self.bucket_name = bucket_name
+        self.public_url = public_url
+        self.s3 = boto3.client('s3',
+            endpoint_url=endpoint_url,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name=region_name
+        )
+
+    def is_object_exists(self, object_name) -> bool:
+        return object_name in self.list()
+
+    def upload(self, file_path, object_name) -> str:
+        file_path = Path(file_path)
+        if not file_path.is_file():
+            raise ValueError("Path is not a file")
+        self.s3.upload_file(str(file_path), self.bucket_name, object_name)
+        if(self.public_url):
+            return f"{self.public_url}/{object_name}"
+        else:
+            return f"{object_name}"
+
+    def download(self, object_name, file_path) -> None:
+        if not self.is_object_exists(object_name):
+            raise ValueError("Object does not exist in bucket")
+        self.s3.download_file(self.bucket_name, object_name, file_path)
+
+    def list_objects(self) -> list[str]:
+        response = self.s3.list_objects_v2(Bucket=self.bucket_name)
+        objects = []
+        while True:
+            for obj in response.get('Contents', []):
+                objects.append(obj)
+            if response.get('IsTruncated'):
+                response = self.s3.list_objects_v2(Bucket=self.bucket_name, ContinuationToken=response['NextContinuationToken'])
+            else:
+                break
+        return objects
+
+    def list(self) -> list[str]:
+        return [obj['Key'] for obj in self.list_objects()]
+
+    def delete(self, object_name) -> None:
+        if not self.is_object_exists(object_name):
+            raise ValueError("Object not found in bucket")
+        self.s3.delete_object(Bucket=self.bucket_name, Key=object_name)
